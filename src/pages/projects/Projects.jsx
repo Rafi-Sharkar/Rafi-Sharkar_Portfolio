6cdfe2ea0d9331cd8f0449b51c4a2870fe25477e
Mustakim Billah Rafi <rafisharkar144@gmail.com>
2024-04-30 12:51:44 +0600

    added project page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Footer from '../../global components/footer/Footer'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <div className='w-full flex justify-center bg-gray-400 text-white'>
+        <h2 className='text-4xl py-3 text-center'>Projects</h2>
+      </div>
+      <div className='flex flex-wrap justify-center border-2 my-3 mx-1'>
+        Here are all projects that I made
+      </div>
+      <Footer />
+    </div>
   )
 }
